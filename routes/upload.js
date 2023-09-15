b653d6fa80928dbf48a82fbb53b987be00f6c3cd
Hamza Zahid <zahidhamza001@gmail.com>
2023-09-15 09:13:28 -0700

    some changes

--- routes/upload.js
+++ routes/upload.js
@@ -9,19 +9,21 @@ const {
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 const bucketName = process.env.BUCKET_NAME
-const bucketRegion = process.env.AWS_REGION
-const accessKey = process.env.AWS_ACCESS_KEY_ID
-const secretKey = process.env.AWS_SECRET_ACCESS_KEY
-const sessionT = process.env.AWS_SESSION_TOKEN
+// const bucketRegion = process.env.AWS_REGION
+// const accessKey = process.env.AWS_ACCESS_KEY_ID
+// const secretKey = process.env.AWS_SECRET_ACCESS_KEY
+// const sessionT = process.env.AWS_SESSION_TOKEN
 
-const s3 = new S3Client({
-  region: bucketRegion,
-  credentials: {
-    accessKeyId: accessKey,
-    secretAccessKey: secretKey,
-    sessionToken: sessionT,
-  },
-});
+const s3 = new S3Client();
+//   {
+//   region: bucketRegion,
+//   credentials: {
+//     accessKeyId: accessKey,
+//     secretAccessKey: secretKey,
+//     sessionToken: sessionT,
+//   },
+// }
+
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
