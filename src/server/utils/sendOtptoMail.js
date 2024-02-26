fb64ee0768d8b277a3fdf7206d2d7995c2d22905
Hamza Zahid <zahidhamza001@gmail.com>
2024-02-26 16:50:44 +0500

    Await for mail to be sent

--- src/server/utils/sendOtptoMail.js
+++ src/server/utils/sendOtptoMail.js
@@ -11,35 +11,38 @@ async function sendOtpToEmail(reseverEmail, token) {
     secureConnection: false,
     auth: {
       user: process.env.AUTH_MAIL,
-      pass: process.env.AUTH_PASSWORD
+      pass: process.env.AUTH_PASSWORD,
     },
     tls: {
-      rejectUnauthorized: true
-    }
+      rejectUnauthorized: true,
+    },
   })
 
   let mailOptions = {
     from: process.env.AUTH_MAIL,
     to: reseverEmail,
     subject: "Quiz App: Verify your email",
-    html: `<h2 style="color:black;text-align:center;">Quiz App send you email for otp varification<br>Your OTP is here <br> <b> <a href="${process.env.WEB_URL}/api/v1/user/verify?token=` + token +`">Link Here</a>  </b> </h2>`
+    html:
+      `<h2 style="color:black;text-align:center;">Quiz App send you email for otp varification<br>Your OTP is here <br> <b> <a href="${process.env.WEB_URL}/api/v1/user/verify?token=` +
+      token +
+      `">Link Here</a>  </b> </h2>`,
   }
-  
+
   // send otp to email
-  transporter.sendMail(mailOptions, async function (error, info) {
-    if (error) {
-      console.log({ error })
-      
-      // throw new ApiError(500, "send otp to email error")
-    } else {
-      console.log(info.response)
-    }
+
+  const sendMail = new Promise((resolve) => {
+    transporter.sendMail(mailOptions, async function (error, info) {
+      resolve({ error, info })
+    })
   })
+
+  const { error, info } = await sendMail
+
+  if (error) {
+    console.log({ error })
+  } else {
+    console.log(info.response)
+  }
 }
 
 export { sendOtpToEmail }
-
-
-
-
-
